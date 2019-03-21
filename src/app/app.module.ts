8ac2ce319f02400f19f8acc829fb04a5fee0e3ed
JinHyeChoi <nicjh77@gmail.com>
2019-03-20 20:17:54 -0400

    Add Authentication(Login, interceptor, home)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { ReactiveFormsModule } from "@angular/forms";
-import { HttpClientModule } from "@angular/common/http";
+import { ReactiveFormsModule, FormsModule } from "@angular/forms";
+import { HttpClientModule, HTTP_INTERCEPTORS } from "@angular/common/http";
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ToastrModule } from 'ngx-toastr';
 
@@ -11,25 +11,33 @@ import { UserComponent } from './user/user.component';
 import { LoginComponent } from './user/login/login.component';
 import { RegistrationComponent } from './user/registration/registration.component';
 import { UserService } from './shared/user.service';
+import { HomeComponent } from './home/home.component';
+import { AuthInterceptor } from './auth/auth.interceptor';
 
 @NgModule({
   declarations: [
     AppComponent,
     UserComponent,
     LoginComponent,
-    RegistrationComponent
+    RegistrationComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     ReactiveFormsModule,
+    FormsModule,
     HttpClientModule,
     BrowserAnimationsModule,
     ToastrModule.forRoot({
       progressBar: true
     })
   ],
-  providers: [UserService],
+  providers: [UserService, {
+    provide: HTTP_INTERCEPTORS,
+    useClass: AuthInterceptor,
+    multi: true
+  }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/user/login/login.component.ts
+++ src/app/user/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { UserService } from 'src/app/shared/user.service';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +11,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  formModel={
+    UserName: '',
+    Password: ''
+  }
+  constructor(private service: UserService, private router: Router, private toastr: ToastrService) { }
 
   ngOnInit() {
+    if(localStorage.getItem('token') != null)
+      this.router.navigateByUrl('/home');
   }
 
+  onSubmit(form: NgForm){
+    this.service.login(form.value).subscribe(
+      (res:any)=>{
+        localStorage.setItem('token', res.token);
+        this.router.navigateByUrl('/home');   // 로그인 하면서 토큰을 리턴받아 저장 후 홈으로 이동
+      },
+      err=>{
+        if(err.status == 400)
+          this.toastr.error('Incorrect username or password', 'Authentication failed');
+        else
+          console.log(err);
+      }
+    );
+  }
 }
